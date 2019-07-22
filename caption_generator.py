9636e090322ba4da8137e1d62c4b9657d327b5b7
aditya.sharma <aditya.sharma@plasticmobile.com>
2019-07-22 09:32:03 -0400

    download working now using pysplashgit add caption_generator.py

--- caption_generator.py
+++ caption_generator.py
@@ -1,45 +1,37 @@
 # pull a bunch of images from unsplash using their API
 
-
 import os
 import sys
-from pixabay import Image
 import urllib.request
-import pprint
-
-
-
+import logging
+from pyunsplash import PyUnsplash
 
 def main():
 
-  pp = pprint.PrettyPrinter(indent=4)
+  # api 'tings
+  api_key = "cd9ad8287959d3408ee5c1db5486114901cc1a7aa0c19c31529030b78204be51"
+  # client_secret = "c8eab14f9a07851496021a521635606ea2c233720fc341b7b76aca0739c48d16"
+  # redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
 
+  #authorization code
+  # code = "9c218e06dedd4f11ddb108a73f86f8ac9f5cdfcef90b92a521e92d7c1378bd0b"
+  py_un = PyUnsplash(api_key=api_key)
   # category = ['travel', 'animals', 'nature', 'fashion']
+
   category = [ 'food', 'computer', 'sports', 'fashion']
-  lookup = {'food': 'nice', 'computer': 'cool', 'sports': 'fun', 'fashion': 'trendy'}
-  # api_key = '13065241-0418ed825f53b0418b9b2810a'
-  api_key = '13065996-ad3c04acd5537b6de9dcbea87'
-
-  image = Image(api_key)
-
+  # lookup = {'food': 'nice', 'computer': 'cool', 'sports': 'fun', 'fashion': 'trendy'}
+  print(py_un)
   for i in category:
-    image_response = image.search(
-      q=i + ' '+lookup.get(i),
-      lang='es',
-      image_type='photo',
-      orientation='horizontal',
-      category= i,
-      safesearch='true')
-    pp.pprint(image_response)
-    prev_image = image_response['hits'][0]['previewURL']
-      # print(i + lookup.get(i))
-    # urllib.request.urlretrieve(image_response['hits'][0]['previewURL'], 'testing' + i + '.jpg')
+    q = 'beautiful ' + i
+    print(q)
 
-  urllib.request.urlretrieve(prev_image, "00000001.jpg")
+    photos = py_un.search(type_='photos', query = q)
 
-      # print(image_response)
-      # print(image_response['hits'][0]['previewURL'])
-
+    print (photos)
+    for photo in photos.entries:
+      full_path = os.getcwd() + '/' + i + '_' + photo.id + '.jpg'
+      print(full_path)
+      urllib.request.urlretrieve(photo.link_download, full_path)
 
 
 
